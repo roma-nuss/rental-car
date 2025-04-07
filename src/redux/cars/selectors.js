c9a27ca739ef30a0854bba83a8ad9efa2eebdc0f
Roman <romka22830@gmail.com>
2025-04-07 04:23:35 +0300

    update

--- src/redux/cars/selectors.js
+++ src/redux/cars/selectors.js
@@ -1,11 +1,3 @@
-// export const selectCarCatalog = state => state.cars.catalog;
-// export const selectCarBrands = state => state.cars.brands;
-// export const selectFavoriteCarList = state => state.cars.favoriteCars;
-// export const selectTotalPageCount = state => state.cars.totalPages;
-// export const selectCurrentPage = state => state.cars.page;
-// export const selectItemsPerPage = state => state.cars.limit;
-// export const selectLoadingState = state => state.cars.loading;
-
 export const selectCars = state => state.cars.catalog;
 export const selectBrands = state => state.cars.brands;
 export const selectFavoriteCars = state => state.cars.favoriteCars;
